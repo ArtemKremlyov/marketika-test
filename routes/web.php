b383df7d99cb7fd0e65abf5bad08721d0d0bbd20
ArtemKremlyov <54173378+ArtemKremlyov@users.noreply.github.com>
2021-03-22 18:52:57 +0700

    created books resource controller, book model and register books route(admin)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::resource('admin.authors', AuthorsController::class);
+Route::resource('admin.books', BooksController::class);
